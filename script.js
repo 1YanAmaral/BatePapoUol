7e734cabe643d8ed4bee24e5dd4df26e22f0fff6
Yan <yanc.amaral@gmail.com>
2022-06-27 11:13:39 -0300

    requisitos + bonus tela de entrada

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 let username;
 
-function askUsername(){
-    username = {
-        name: prompt("Qual o seu lindo nome?")
-    };
-}
-
-askUsername();
 
 function sendUsername(){
+    username = {
+        name: document.querySelector(".userName").value
+    };
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", username);
     promise.catch(catchError);
+    promise.then(hideLoginScreen);
     promise.then(getMessages);
 }
 
-sendUsername();
 
 function catchError(error) {
     if (error.response.status === 400) {
         alert("Nome de usuário indisponível!");
-        askUsername();
+        
     }
 }
 
@@ -39,15 +35,15 @@ function printMessages() {
     messageBoard.innerHTML = "";
      for (let i = 0; i < msgContent.length ; i++) {
         if (msgContent[i].type === "status") {
-            let newMessage = `<div class="message typeStatus"><span class="time">(${msgContent[i].time})</span> <span class="strong"> ${msgContent[i].from} </span> ${msgContent[i].text}</div>`;
+            let newMessage = `<div class="message typeStatus"><span class="time">(${msgContent[i].time})</span> <span class="from"> ${msgContent[i].from} </span> ${msgContent[i].text}</div>`;
             messageBoard.innerHTML += newMessage;            
         }
         if (msgContent[i].type === "private_message" && msgContent[i].to === username) {
-            let newMessage = `<div class="message typePrivate"><span class="time">(${msgContent[i].time})</span> <span class="strong"> ${msgContent[i].from}</span> para <span class="strong">${msgContent[i].to}: </span>${msgContent[i].text}</div>`;
+            let newMessage = `<div class="message typePrivate"><span class="time">(${msgContent[i].time})</span> <span class="from"> ${msgContent[i].from}</span> para <span class="to">${msgContent[i].to}: </span>${msgContent[i].text}</div>`;
             messageBoard.innerHTML += newMessage;
         }
         if (msgContent[i].type === "message") {
-            let newMessage = `<div class="message"><span class="time">(${msgContent[i].time})</span> <span class="strong"> ${msgContent[i].from} </span> para <span class="strong"> ${msgContent[i].to}:</span> ${msgContent[i].text}</div>`;
+            let newMessage = `<div class="message"><span class="time">(${msgContent[i].time})</span> <span class="from"> ${msgContent[i].from} </span> para <span class="to"> ${msgContent[i].to}:</span> ${msgContent[i].text}</div>`;
             messageBoard.innerHTML += newMessage;
         }
      }
@@ -58,7 +54,7 @@ setInterval(getMessages, 3000);
 setInterval(sustainConnection, 3000);
 
 function sendMessage() {
-    const msg = document.querySelector("input").value;
+    const msg = document.querySelector(".msg").value;
     const objMessage = {
         from: username.name,
         to: "Todos",
@@ -66,7 +62,6 @@ function sendMessage() {
         type: "message"         
     };
 
-    console.log(objMessage);
     let promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', objMessage);
     promise.then(getMessages);
     promise.catch(loadPage);
@@ -83,3 +78,46 @@ function sustainConnection() {
     
 }
 
+function showParticipants() {
+    document.querySelector(".options").classList.remove("hidden");
+    document.querySelector(".background").classList.remove("hidden");
+
+}
+
+function hideParticipants () {
+    document.querySelector(".options").classList.add("hidden");
+    document.querySelector(".background").classList.add("hidden");
+}
+
+function getParticipants(){
+    let promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promise.then(storeParticipants);   
+    
+}
+
+function storeParticipants(object) {
+    objContent = object.data;
+    printParticipants();
+    
+}
+
+function printParticipants() {
+    let participantsList = document.querySelector("ul");
+    participantsList.innerHTML = `<li><ion-icon name="people"></ion-icon>Todos</li>`;
+    for (let i = 0; i <= objContent.length ; i++) {
+        participantsList.innerHTML += `<li><ion-icon name="person-circle"></ion-icon>${objContent[i].name}</li>`
+        
+    }
+   
+}
+
+function hideLoginScreen (){
+    document.querySelector(".loginScreen").classList.add("hidden");
+}
+
+function showLoader (){
+    document.querySelector(".loader").classList.remove("hidden");
+    document.querySelector(".loginIn").classList.remove("hidden");
+    document.querySelector(".userName").classList.add("hidden");
+    document.querySelector("button").classList.add("hidden");
+}
